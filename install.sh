94f13650fc2c5a93989542a506882342d82cc114
steigerbalett <40899113+steigerbalett@users.noreply.github.com>
2022-01-16 10:37:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo '                   GNU GENERAL PUBLIC LICENSE'
 echo ''
 echo '
     FHEM-RPi-install
-    Copyright (c) 2022 steigerbalett
+    Copyright (c) 2019-2022 steigerbalett
 
     This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 
